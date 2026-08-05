5cd1dd73c3198fd702bed6d9b52e2e54f22a9cde
suwu2004 <suwu4467@gmail.com>
2026-08-05 17:54:48 +0800

    Restore shared reading home icon path

--- src/Root.jsx
+++ src/Root.jsx
@@ -50,7 +50,7 @@ function ReadingHomeEntry({ onOpen }) {
     <button className="home-room-app home-room-app--reading" type="button" onClick={onOpen} aria-label="打开共读小屋">
       <span>
         <svg className="home-room-glyph" viewBox="0 0 36 36" aria-hidden="true">
-          <path d="M5.5 8.5h10c1.8 0 2.5 1 2.5 2.8v17.2c0-1.8-.7-2.8-2.5-2.8h-10V8.5Zm25 0h-10c-1.8 0-2.5 1-2.5 2.8v17.2c0-1.8.7-2.8-2.5-2.8h-10V8.5Z" />
+          <path d="M5.5 8.5h10c1.8 0 2.5 1 2.5 2.8v17.2c0-1.8-.7-2.8-2.5-2.8h-10V8.5Zm25 0h-10c-1.8 0-2.5 1-2.5 2.8v17.2c0-1.8-.7-2.8-2.5-2.8h-10V8.5Z" />
           <path d="M9 13h5M22 13h5M9 17h5M22 17h5" />
         </svg>
       </span>
