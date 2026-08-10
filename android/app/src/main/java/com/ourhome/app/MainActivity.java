119fe86d57b4c3c5ef25856b598ef22e5400005f
suwu2004 <suwu4467@gmail.com>
2026-08-10 12:10:41 +0800

    Register native updater plugin

--- android/app/src/main/java/com/ourhome/app/MainActivity.java
+++ android/app/src/main/java/com/ourhome/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ourhome.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(OurHomeUpdaterPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
